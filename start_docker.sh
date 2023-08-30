810bd1a325524ea51420dc785396decc2193ef69
stevenHust <huzhiyang4555@gmail.com>
2023-08-30 20:38:38 +0800

    Update start_docker.sh

--- start_docker.sh
+++ start_docker.sh
@@ -7,15 +7,9 @@ base=`dirname "$0"`
 base=`cd "$base"; pwd`
 cd ${base}
 
-image_version=aglimage/agl:agl-ubuntu-gcc9.4.0-py3.8-cuda11.8-pytorch2.0.1-0825
+docker pull aglimage/agl:agl-ubuntu-gcc9.4.0-py3.8-cuda11.8-pytorch2.0.1-0825
 
-if [[ -z $(docker images -q ${image_version}) ]];
-then
-    docker pull ${image_version}
-fi
-
-docker run --net=host --rm -it -m 30000m \
-    -v ${base}/..:/graph_ml \
+docker run -v ${base}/..:/graph_ml \
     -w /graph_ml \
-    ${image_version} \
+    aglimage/agl:agl-ubuntu-gcc9.4.0-py3.8-cuda11.8-pytorch2.0.1-0825 \
     "/bin/bash"
